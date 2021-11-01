172f585e3c6309be65f1aa5c09524248b032a486
unknown <gallardoirg@gmail.com>
2021-11-01 05:16:58 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 5137490,
+    "boundingBox": {
+        "lx": 451434.5728143332,
+        "ly": 2295076.781930822,
+        "lz": -18.405,
+        "ux": 454069.25381433308,
+        "uy": 2297711.462930822,
+        "uz": 2616.2759999998657
+    },
+    "tightBoundingBox": {
+        "lx": 451434.5728143332,
+        "ly": 2295076.781930822,
+        "lz": -18.405,
+        "ux": 453605.1918143332,
+        "uy": 2297711.462930822,
+        "uz": 170.537
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 22.817007064819337,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
